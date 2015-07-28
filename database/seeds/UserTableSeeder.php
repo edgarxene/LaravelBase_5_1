2dc33162bbdc9d31d420f0b8476876d807c4f7af
Edgar Cruz <edgarxene@gmail.com>
2015-07-27 22:48:51 -0500

    Commit inicial

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users') ->truncate();
+    	factory(App\User::class)->create([
+        	'name' => 'Edgar',
+        	'role' => 'admin'
+        	]);
+        factory(App\User::class, 9)->create();
+    }
+}
